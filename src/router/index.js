8f418bb936fd018c7ec6eae497cddbb2508288a6
Martita Apel <mtapel@uc.cl>
2020-08-28 11:47:12 -0400

    guards router

--- src/router/index.js
+++ src/router/index.js
@@ -2,32 +2,51 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 const List = () => import("../views/List.vue");
+const NotFound = () => import("../views/NotFound.vue");
+
+import firebase from "firebase";
 
 Vue.use(VueRouter);
-
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/list",
-    name: "List",
-    component: List,
-  },
-  {
-    path: "/about",
-    name: "About",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes,
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: Home,
+    },
+    {
+      path: "/list",
+      name: "List",
+      component: List,
+      meta: {
+        login: true,
+      },
+    },
+    {
+      path: "/about",
+      name: "About",
+      component: () =>
+        import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    },
+    {
+      path: "*",
+      name: "NotFound",
+      component: NotFound,
+    },
+  ],
+});
+
+router.beforeEach((to, from, next) => {
+  let user = firebase.auth().currentUser;
+  let authRequired = to.matched.some((route) => route.meta.login);
+  if (!user && authRequired) {
+    next("/");
+  } else {
+    next();
+  }
 });
 
 export default router;
---
+++ src/views/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="notfound">
+    <h1>Not FOund</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+  components: {},
+};
+</script>
+
+<style scoped lang="scss"></style>
