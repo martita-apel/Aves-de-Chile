1e8eed110083810c6a5925aa3e9a5bb8f7359e7b
Martita Apel <mtapel@uc.cl>
2020-08-28 13:10:57 -0400

    list cards

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,20 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     currentUser: undefined,
+    birds: [],
   },
   mutations: {
     UPDATE_CURR_USER(state, user) {
       state.currentUser = user;
+    },
+    GET_BIRDS(state, birds) {
+      state.birds = birds;
     },
   },
   actions: {
@@ -22,6 +27,17 @@ export default new Vuex.Store({
           reject(e);
         }
       });
+    },
+    getBirds({ commit }) {
+      axios
+        .get("http://aves.ninjas.cl/api/birds")
+        .then((response) => {
+          console.log(response.data);
+          commit("GET_BIRDS", response.data);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
     },
   },
   modules: {},
--- src/views/List.vue
+++ src/views/List.vue
@@ -1,14 +1,62 @@
 <template>
   <div class="list">
     <h1>Lista de aves</h1>
+    <div>
+      <b-form-input v-model="text" placeholder="Enter your name"></b-form-input>
+      <div class="mt-2">Value: {{ text }}</div>
+      <b-button pill variant="info">Button</b-button>
+    </div>
+
+    <b-container class="bv-example-row mb-3">
+      <b-row cols="6" class="cartas">
+        <b-card
+          class="m-2 card"
+          v-for="bird in birds"
+          :key="bird.id"
+          :img-src="bird.images.main"
+          img-alt="Image"
+          img-top
+        >
+          <b-card-title class="titulo">{{ bird.name.spanish }}</b-card-title>
+        </b-card>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import { mapState, mapActions } from "vuex";
 
 export default {
   name: "List",
   components: {},
+  data() {
+    return {
+      text: "",
+    };
+  },
+  computed: {
+    ...mapState(["birds"]),
+  },
+  methods: {
+    ...mapActions(["getBirds"]),
+  },
+  created() {
+    this.getBirds();
+  },
 };
 </script>
+
+<style scoped>
+.cartas {
+  justify-content: center;
+}
+.card {
+  max-height: 220px;
+  min-height: 150px;
+}
+.titulo {
+  font-size: 15px;
+  padding: 0;
+}
+</style>
